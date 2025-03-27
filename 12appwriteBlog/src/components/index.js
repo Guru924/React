012836a264d04baf98afdbe4fa68d11f4c6a53ff
Guru924 <guruprasaddas924@gmail.com>
2025-03-27 08:37:42 +0530

    BlogApp project with appwrite

---
+++ 12appwriteBlog/src/components/index.js
@@ -0,0 +1,29 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Input from "./Input";
+import Login from "./Login";
+import Button from "./Button";
+import Logo from "./Logo";
+import PostCard from "./PostCard"
+import RTE from "./RTE"
+import SelectBtn from "./SelectBtn"
+import SignUp from "./SignUp"
+import Container from "./Container/Container";
+import PostForm from "./post-form/PostForm"
+import AuthLayout from "./AuthLayout"
+
+export {
+    Container,
+    Footer,
+    Header,
+    PostForm,
+    AuthLayout,
+    Button,
+    Input,
+    Login,
+    Logo,
+    PostCard,
+    RTE,
+    SelectBtn,
+    SignUp,
+} 
